23af0c61dc2912e2ded3be1fafd58530a158966f
Muyuze <muyuze9999@gmail.com>
2015-07-20 13:16:53 +0200

    Include the R plot command

--- RT.r
+++ RT.r
@@ -1,6 +1,4 @@
-sayHello <- function(){
-    print('hello')
-}
-
-sayHello()
-
+plot.new()
+plot(-4:4,-4:4,type="n")
+points(rnorm(200),rnorm(200),col="red")
+points(rnorm(100)/2,rnorm(100)/2,col="blue")
